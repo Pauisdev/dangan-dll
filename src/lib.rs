54063c6ab70e5222ee174bcfd10ceda66ab374bc
fabrisdev <ezepuntocom@hotmail.com>
2026-05-26 10:16:06 -0300

    Rendering position in UI

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod ui;
+
 use std::{
     ptr::null,
     thread::{self, sleep},
@@ -6,41 +8,44 @@ use std::{
 
 use windows_sys::Win32::System::LibraryLoader::GetModuleHandleW;
 
+use crate::ui::APP;
+
 const DLL_PROCESS_ATTACH: u32 = 1;
 const _DLL_PROCESS_DETACH: u32 = 0;
 
 #[unsafe(no_mangle)]
-extern "system" fn DllMain(h_instance: u32, fwd_reason: u32, _: *mut core::ffi::c_void) {
+extern "system" fn DllMain(_h_instance: u32, fwd_reason: u32, _: *mut core::ffi::c_void) {
     match fwd_reason {
-        DLL_PROCESS_ATTACH => init(h_instance),
+        DLL_PROCESS_ATTACH => init(),
         _ => {}
     }
 }
 
-fn init(h_instance: u32) {
-    //let dialogue_addr = 0x2FD2B0;
-    //thread::spawn(move || do_hacky_wacky(h_instance));
-    unsafe {
-        let base_address = GetModuleHandleW(null()) as u32;
-        let pos_address = base_address + 0x33cc80;
-        let position_ptr = pos_address as *const Vec3;
-        let position = *position_ptr;
-        println!("Pos: {position:?}");
-    }
-
-    println!("Hello from Danganronpa!");
+fn spawn_ui() {
+    ui::show().expect("Failed to start UI")
 }
 
-fn do_hacky_wacky(h_instance: u32) {
-    /*loop {
-        let addr = h_instance + 0x33cc80;
-        let position_ptr = addr as *const Vec3;
-        unsafe {
-            let value = *position_ptr;
-            println!("Position: {:?}", value);
-        }
+fn init() {
+    println!("Hello from Danganronpa!");
+    thread::spawn(spawn_ui);
+    let base_address = unsafe { GetModuleHandleW(null()) as u32 };
+    let set_player_pos_fn_address = base_address + 0x33cc80;
+    let fn_ptr = set_player_pos_fn_address as *const fn(x: f32, y: f32, z: f32, rotation: f32);
+    let set_player_pos = unsafe { *fn_ptr };
+    thread::spawn(move || read_position(base_address));
+}
+
+fn read_position(base_address: u32) {
+    let pos_address = base_address + 0x33cc80;
+    let position_ptr = pos_address as *const Vec3;
+    loop {
+        let position = unsafe { *position_ptr };
+        APP.position.lock().unwrap().x = position.x;
+        APP.position.lock().unwrap().y = position.y;
+        APP.position.lock().unwrap().z = position.z;
+        println!("Pos: {position:?}");
         sleep(Duration::from_secs(1));
-    }*/
+    }
 }
 
 #[repr(C)]
@@ -49,4 +54,14 @@ struct Vec3 {
     x: f32,
     y: f32,
     z: f32,
+}
+
+impl Vec3 {
+    fn one() -> Vec3 {
+        Vec3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        }
+    }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,1 +1,56 @@
-pub fn start_ui() {}
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::{EventLoopBuilderHook, egui};
+use once_cell::sync::Lazy;
+use std::sync::Arc;
+use std::sync::Mutex;
+use winit::platform::windows::EventLoopBuilderExtWindows;
+
+use crate::Vec3;
+
+pub static APP: Lazy<MyApp> = Lazy::new(|| MyApp {
+    position: Arc::new(Mutex::new(Vec3::one())),
+});
+
+pub fn show() -> eframe::Result {
+    let event_loop_builder: Option<EventLoopBuilderHook> = Some(Box::new(|event_loop_builder| {
+        event_loop_builder.with_any_thread(true);
+    }));
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([200.0, 100.0]),
+        event_loop_builder,
+        ..Default::default()
+    };
+    let app = MyApp {
+        position: APP.position.clone(),
+    };
+    eframe::run_native("Mod helper", options, Box::new(|_cc| Ok(Box::new(app))))
+}
+
+pub struct MyApp {
+    pub position: Arc<Mutex<Vec3>>,
+}
+
+impl eframe::App for MyApp {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show_inside(ui, |ui| {
+            ui.label("Position");
+            ui.horizontal(|ui| {
+                ui.add(egui::DragValue::new(&mut self.position.lock().unwrap().x));
+                ui.label("x");
+                ui.add(egui::DragValue::new(&mut self.position.lock().unwrap().y));
+                ui.label("y");
+                ui.add(egui::DragValue::new(&mut self.position.lock().unwrap().z));
+                ui.label("z");
+            });
+
+            if ui.button("Reset position").clicked() {
+                let mut pos = self.position.lock().unwrap();
+                pos.x = 0.0;
+                pos.y = 0.0;
+                pos.z = -263.0;
+            }
+            ui.request_repaint_after_secs(1.0);
+        });
+    }
+}
